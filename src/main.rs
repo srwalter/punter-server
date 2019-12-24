3dc5aadb7ee7953ba02add5b570448e206823dd6
Steven Walter <stevenrwalter@gmail.com>
2019-12-24 14:40:37 -0500

    Implement file chooser

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,42 @@ use tokio::prelude::*;
 
 async fn handle_client(mut conn: TcpStream) -> io::Result<()> {
     let (read, mut write) = conn.split();
-    let bufread = BufReader::new(read);
-    let mut l = bufread.split('\r' as u8);
+    let mut bufread = BufReader::new(read);
+    let mut transfer = None;
+
     loop {
-        if let Some(line) = l.next_segment().await? {
-            println!("{:?}", line);
-            write.write_all(&line).await?;
-        } else {
-            break;
+        let mut line = vec![128];
+
+        bufread.read_until('\r' as u8, &mut line).await?;
+
+        println!("{:?}", line);
+        let strline = std::str::from_utf8(&line).expect("UTF decode");
+        println!("{}", strline);
+        write.write_all(&line).await?;
+        write.write_all(&"\r".as_bytes()).await?;
+
+        match &strline {
+            &"BYE" => {
+                break;
+            }
+            &"GET" => {
+                write.write_all(&" WHICH FILE?\r".as_bytes()).await?;
+                bufread.read_until('\r' as u8, &mut line).await?;
+                let fname = std::str::from_utf8(&line).expect("UTF decode");
+                transfer = Some(fname.to_string());
+                break;
+            }
+            _ => {
+                write.write_all(&" WAT?\r".as_bytes()).await?;
+            }
         }
     }
+
+    if let Some(fname) = transfer {
+        println!("Transferring {}", fname);
+        write.write_all(&" SENDING NOW\r".as_bytes()).await?;
+    }
+
     Ok(())
 }
 
